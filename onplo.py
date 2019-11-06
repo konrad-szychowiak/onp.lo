4fa18803d25a70527afdc4a6b2161bee9de3f25b
aleksander002 <27806518+aleksander002@users.noreply.github.com>
2019-11-06 17:50:07 +0100

    Add files via upload
    Zalążek onplo (nawet ciekawie brzmi), wczytywanie linia po linii

---
+++ onplo.py
@@ -0,0 +1,8 @@
+#onp.lo - All Rights Reserved.
+import sys
+for postfix in sys.stdin: #nieskonczona petla 
+    postfix = postfix.strip()
+    for i in range(len(postfix)-1, -1, -1): #odczytywanie str postfixa od konca
+        #todo: bufor operacji/przestrzen  nazw/interpretacja (rekurencja?)
+        
+        
