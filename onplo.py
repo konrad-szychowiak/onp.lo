274d6d26088d3cce81691b2ad5844227a7090189
aleksander002 <27806518+aleksander002@users.noreply.github.com>
2019-11-06 17:58:36 +0100

    Update onplo.py
    ...wczytywanie wyrazów będzie lepsze niż znak po znaku...

--- onplo.py
+++ onplo.py
@@ -1,8 +1,9 @@
 #onp.lo - All Rights Reserved.
 import sys
 for postfix in sys.stdin: #nieskonczona petla 
-    postfix = postfix.strip()
-    for i in range(len(postfix)-1, -1, -1): #odczytywanie str postfixa od konca
+    postfix = postfix.strip().split() 
+    for i in range(len(postfix)-1, -1, -1): #odczytywanie wyrazow postfixa od konca
+        
         #todo: bufor operacji/przestrzen  nazw/interpretacja (rekurencja?)
         
         
