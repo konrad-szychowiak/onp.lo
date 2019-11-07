f447dd4c808c4d270955b7a5698056f15e2b317e
aleksander002 <27806518+aleksander002@users.noreply.github.com>
2019-11-07 19:03:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 #onp.lo - All Rights Reserved.
+class Term:
+    def __init__(self, name):
+        self.name = name
+    def get():
+        return f"{self.name}"
+
+
 import sys
 for postfix in sys.stdin: #nieskonczona petla 
     postfix = postfix.strip().split() 
     for i in range(len(postfix)-1, -1, -1): #odczytywanie wyrazow postfixa od konca
         
-        #todo: bufor operacji/przestrzen  nazw/interpretacja (rekurencja?)
+        
         
         
