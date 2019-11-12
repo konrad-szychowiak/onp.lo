4076da7082f908926543b9c37e0cda1f42b1e945
Karina Szubert <52202473+Karina-00@users.noreply.github.com>
2019-11-12 18:32:56 +0100

    Squash and merge new tests

--- main.test.py
+++ main.test.py
@@ -12,9 +12,16 @@ class TestExampleOutput(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_2(self):
-        # actual, expected = onplo.read.get("<input>"), "<output>"
-        # self.assertEqual(expected, actual)
-        pass
+        actual, expected = onplo.read.get("Z Z p/1 EXISTS"), "(EXISTS Z p(Z))"
+        self.assertEqual(expected, actual)
+
+    def test_3(self):
+        actual, expected = onplo.read.get("Z X X a f/2 p/1 ∃ Y Y Z f/1 p/2 FORALL → FORALL"), "(FORALL Z ((∃ X p(f(X, a))) → (FORALL Y p(Y, f(Z)))))"
+        self.assertEqual(expected, actual)
+
+    def test_4(self):
+        actual, expected = onplo.read.get("Z Y X X b c q/3 Z Y f/1 p/2 ~ & EXISTS FORALL EXISTS"), "(EXISTS Z (FORALL Y (EXISTS X (q(X, b, c) & (~ p(Z, f(Y)))))))"
+        self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
