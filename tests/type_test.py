068c1c09ef033fa4287bdc69e52b3ae22928613d
Konrad Szychowiak <konrad.szychowiak@gmail.com>
2019-12-28 19:05:07 +0100

    Fix tests

--- tests/type_test.py
+++ tests/type_test.py
@@ -8,34 +8,34 @@ from onplo import conf
 class TestTermChecks(unittest.TestCase):
 
     def test_type_predicate(self):
-        actual = type.predicate("p/1")
+        actual = type.rematch("predicate", "p/1")
         self.assertTrue(actual)
 
     def test_type_function(self):
-        actual = type.function("f/1")
+        actual = type.rematch("function", "f/1")
         self.assertTrue(actual)
 
     def test_type_variable(self):
-        actual = type.variable("X")
+        actual = type.rematch("variable", "X")
         self.assertTrue(actual)
 
     def test_type_constant(self):
-        actual = type.constant("c")
+        actual = type.rematch("constant", "c")
         self.assertTrue(actual)
 
 
 class TestOperandChecks(unittest.TestCase):
 
     def test_type_quantifier(self):
-        actual = type.quantifier("FORALL")
+        actual = type.match("quantifier", "FORALL")
         self.assertTrue(actual)
 
     def test_type_single(self):
-        actual = type.single("NOT")
+        actual = type.match("single", "NOT")
         self.assertTrue(actual)
 
     def test_type_double(self):
-        actual = type.double("XOR")
+        actual = type.match("double", "XOR")
         self.assertTrue(actual)
 
 
